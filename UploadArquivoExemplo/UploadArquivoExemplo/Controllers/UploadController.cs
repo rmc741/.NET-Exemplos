1faca6272a6b64610636692f94e03d717dff9a48
Rafael Mendonca <rafael.medonca2@gmail.com>
2024-08-07 15:23:14 -0300

    exemplo upload de arquivo csv.

---
+++ UploadArquivoExemplo/UploadArquivoExemplo/Controllers/UploadController.cs
@@ -0,0 +1,25 @@
+﻿using CsvHelper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace UploadArquivoExemplo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<List<DadosTablet>> TesteUploadArquivoBanco(IFormFile file)
+        {
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<DadosTablet>();
+                //Console.WriteLine(records.ToList().ToString());
+                return records.ToList();
+            }
+        }
+    }
+}
---
+++ UploadArquivoExemplo/UploadArquivoExemplo/DadosTablet.cs
@@ -0,0 +1,24 @@
+﻿namespace UploadArquivoExemplo
+{
+    public class DadosTablet
+    {
+        public int Id { get; set; }
+        public int EditalId { get; set; }
+        public int Inscricao { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public DateTime Dia { get; set; }
+        public DateTime HoraAberturaPortoes { get; set; }
+        public DateTime HoraFechamentoPortoes { get; set; }
+        public DateTime HoraProva { get; set; }
+        public string Concurso { get; set; }
+        public string Cargo { get; set; }
+        public string LocalProva { get; set; }
+        public string Escola { get; set; }
+        public string Andar { get; set; }
+        public string Bloco { get; set; }
+        public string Sala { get; set; }
+        public string Endereco { get; set; }
+        public string? Obs { get; set; }
+    }
+}
